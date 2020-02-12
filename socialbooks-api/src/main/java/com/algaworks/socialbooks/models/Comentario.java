2ef58455f62f60381261bbab636606d4023564bb
Marcos Corsi <marcosvcorsi@gmail.com>
2020-02-11 22:38:41 -0300

    Comments adjustments

--- socialbooks-api/src/main/java/com/algaworks/socialbooks/models/Comentario.java
+++ socialbooks-api/src/main/java/com/algaworks/socialbooks/models/Comentario.java
@@ -1,6 +1,5 @@
 package com.algaworks.socialbooks.models;
 
-import java.time.LocalDate;
 import java.time.LocalDateTime;
 
 import javax.persistence.Entity;
--- socialbooks-api/src/main/java/com/algaworks/socialbooks/repositories/ComentarioRepository.java
+++ socialbooks-api/src/main/java/com/algaworks/socialbooks/repositories/ComentarioRepository.java
@@ -1,11 +1,8 @@
 package com.algaworks.socialbooks.repositories;
 
-import java.util.List;
-
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.algaworks.socialbooks.models.Comentario;
-import com.algaworks.socialbooks.models.Livro;
 
 public interface ComentarioRepository extends JpaRepository<Comentario, Long>{
 
