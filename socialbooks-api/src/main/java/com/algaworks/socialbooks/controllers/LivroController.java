e1b676a799686b15a9025362c47d4daa93a08aa8
Marcos Corsi <marcosvcorsi@gmail.com>
2020-02-12 23:11:12 -0300

    Project finished

--- socialbooks-api/src/main/java/com/algaworks/socialbooks/controllers/LivroController.java
+++ socialbooks-api/src/main/java/com/algaworks/socialbooks/controllers/LivroController.java
@@ -8,8 +8,9 @@ import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.CacheControl;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -76,6 +77,9 @@ public class LivroController {
 	
 	@PostMapping("{id}/comentarios")
 	public ResponseEntity<Comentario> addComment(@PathVariable("id") Long livroId, @RequestBody Comentario comentario) {
+		Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+		comentario.setUsuario(auth.getName());
+		
 		comentario = livroService.saveComentario(livroId, comentario);
 	
 		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().build().toUri();
